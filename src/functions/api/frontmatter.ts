d143b7062b517aee3ee007e5b2bcc4f855ba44d9
Sander Cokart <cokart32@gmail.com>
2023-02-04 19:41:05 +0100

    fixing search

--- src/functions/api/frontmatter.ts
+++ src/functions/api/frontmatter.ts
@@ -2,10 +2,12 @@ import fs from 'fs';
 import m from 'front-matter';
 import {PostFrontMatter} from '@/types/data/FrontMatter';
 
+const root = process.cwd();
+
 export function getPostsWithFrontmatter() {
-    const postFileNames = fs.readdirSync('./src/pages/blog/posts');
+    const postFileNames = fs.readdirSync(`${root}/src/pages/blog/posts`);
     return postFileNames.map((filename) => {
-        const source = fs.readFileSync(`./src/pages/blog/posts/${filename}`, 'utf-8');
+        const source = fs.readFileSync(`${root}/src/pages/blog/posts/${filename}`, 'utf-8');
         const { attributes } = m<PostFrontMatter>(source);
 
         return { ...attributes, slug: filename.replace('.mdx', '') };
