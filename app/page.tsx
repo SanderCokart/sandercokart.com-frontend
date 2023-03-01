87537c1a70e46d79b639d4728ee076cb175ea008
Sander Cokart <cokart32@gmail.com>
2023-03-01 15:44:54 +0100

    Added ref to codehouse.sandercokart.com

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import './globals.css';
 import {Typewriter} from 'react-simple-typewriter';
 import {motion, Variants} from 'framer-motion';
+import Link from 'next/link';
 
 const h1Variants: Variants = {
     hidden: { x: '-100vw' },
@@ -23,7 +24,7 @@ const Page = () => {
 
     return (
         <main className="h-[calc(100%-50px)] grid place-items-center overflow-hidden">
-            <div className="grid grid-rows-[1fr,1fr] gap-8 text-center h-full font-bold">
+            <div className="grid grid-rows-[1fr,1fr,1fr] gap-8 text-center h-full font-bold">
                 <motion.h1 initial="hidden" animate="visible" variants={h1Variants} className="landscape:text-[4vw] portrait:text-[4vh] self-end">SanderCokart.com</motion.h1>
                 <motion.h2 initial="hidden" animate="visible" variants={h2Variants} className="landscape:text-[4vw] portrait:text-[4vh]">
                     <Typewriter typeSpeed={70}
@@ -33,6 +34,7 @@ const Page = () => {
                                 loop
                                 cursor/>
                 </motion.h2>
+                <motion.h1 initial="hidden" animate="visible" variants={h1Variants} className="landscape:text-[3vw] portrait:text-[3vh] self-end">While waiting please visit <Link className="hover:text-violet-600" href="https://codehouse.sandercokart.com">codehouse.sandercokart.com</Link></motion.h1>
             </div>
         </main>
     );
