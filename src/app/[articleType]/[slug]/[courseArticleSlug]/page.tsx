77dddcca4e23c866b80a99d7ccb8d211caf2284f
Sander Cokart <cokart32@gmail.com>
2023-08-06 17:54:21 +0200

    Removed dead page

--- src/app/[articleType]/[slug]/[courseArticleSlug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react';
-
-interface CourseArticlePageProps {}
-
-const CourseArticlePage = (props: CourseArticlePageProps) => {
-  return <></>;
-};
-
-export default CourseArticlePage;
