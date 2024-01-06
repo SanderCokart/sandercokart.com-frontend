dfd22f6a749985ee25c9762e577041ef08264cc8
Sander Cokart <cokart32@gmail.com>
2024-01-06 18:19:18 +0100

    wip

--- src/app/(components)/Slides.tsx
+++ src/app/(components)/Slides.tsx
@@ -1,74 +1,103 @@
-'use client';
+"use client";
 
-import type {ArticleModel, CourseModel} from '@/types/ModelTypes';
+import type { ArticleModel, CourseModel } from "@/types/ModelTypes";
+import type { ReactNode } from "react";
 
-import ArticleFigure from '@/app/(components)/(figures)/ArticleFigure';
-import CourseFigure from '@/app/(components)/(figures)/CourseFigure';
-import {localArticlesRoute, localCoursesRoute} from '@/routes/local-routes';
-import {motion} from 'framer-motion';
-import {SwiperSlide} from 'swiper/react';
+import ArticleFigure from "@/app/(components)/(figures)/ArticleFigure";
+import CourseFigure from "@/app/(components)/(figures)/CourseFigure";
+import { localArticlesRoute, localCoursesRoute } from "@/routes/local-routes";
+import { motion } from "framer-motion";
+import { SwiperSlide } from "swiper/react";
 
-import GenericSwiper from '@/components/Swipers/GenericSwiper';
+import GenericSwiper from "@/components/Swipers/GenericSwiper";
 
 const containerVariants = {
-    hidden: {},
-    show:   {
-        transition: {
-            staggerChildren: .5
-        }
-    }
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
 };
 
 const itemVariants = {
-    hidden: { opacity: 0, transition: { duration: .5 } },
-    show:   { opacity: 1, transition: { duration: .5 } }
+  hidden: { opacity: 0, transition: { duration: 0.5 } },
+  show: { opacity: 1, transition: { duration: 0.5 } },
 };
 
 interface SlidesProps {
-    articles: {
-        general: ArticleModel[];
-        courses: CourseModel[];
-        tips: ArticleModel[];
-    };
+  articles: {
+    general: ArticleModel[];
+    courses: CourseModel[];
+    tips: ArticleModel[];
+  };
 }
 
+const ComingSoon = ({
+  active,
+  children,
+}: {
+  active: boolean;
+  children: ReactNode;
+}) => {
+  if (active)
+    return <div className="text-center font-code text-2xl">Coming Soon...</div>;
+
+  return children;
+};
+
 const Slides = ({ articles: { general, tips, courses } }: SlidesProps) => {
-    return (
-        <motion.div
-            animate="show"
-            className="flex select-none flex-col"
-            initial="hidden"
-            variants={containerVariants}
-        >
-            <motion.div className="relative z-10" variants={itemVariants}>
-                <GenericSwiper moreHref={localArticlesRoute('general')} title="General">
-                    {general.map((article) => (
-                        <SwiperSlide key={article.id} className="aspect-video w-full overflow-hidden border-4 border-primary dark:border-primaryDark sm:w-[min(578px,100%)] md:rounded">
-                            <ArticleFigure article={article}/>
-                        </SwiperSlide>
-                    ))}
-                </GenericSwiper>
-            </motion.div>
-            <motion.div className="relative z-10" variants={itemVariants}>
-                <GenericSwiper moreHref={localCoursesRoute()} title="Courses">
-                    {courses.map((course) => (
-                        <SwiperSlide key={course.id} className="aspect-video w-full overflow-hidden border-4 border-primary dark:border-primaryDark sm:w-[min(578px,100%)] md:rounded">
-                            <CourseFigure course={course}/>
-                        </SwiperSlide>
-                    ))}
-                </GenericSwiper>
-            </motion.div>
-            <motion.div className="relative z-10" variants={itemVariants}>
-                <GenericSwiper moreHref={localArticlesRoute('tips')} title="General">
-                    {tips.map((article) => (
-                        <SwiperSlide key={article.id} className="aspect-video w-full overflow-hidden border-4 border-primary dark:border-primaryDark sm:w-[min(578px,100%)] md:rounded">
-                            <ArticleFigure article={article}/>
-                        </SwiperSlide>
-                    ))}
-                </GenericSwiper>
-            </motion.div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      animate="show"
+      className="flex select-none flex-col"
+      initial="hidden"
+      variants={containerVariants}
+    >
+      <motion.div className="relative z-10" variants={itemVariants}>
+        <GenericSwiper moreHref={localArticlesRoute("general")} title="General">
+          <ComingSoon active={!general.length}>
+            {general.map((article) => (
+              <SwiperSlide
+                key={article.id}
+                className="aspect-video w-full overflow-hidden border-4 border-primary dark:border-primaryDark sm:w-[min(578px,100%)] md:rounded"
+              >
+                <ArticleFigure article={article} />
+              </SwiperSlide>
+            ))}
+          </ComingSoon>
+        </GenericSwiper>
+      </motion.div>
+      <motion.div className="relative z-10" variants={itemVariants}>
+        <GenericSwiper moreHref={localCoursesRoute()} title="Courses">
+          <ComingSoon active={!courses.length}>
+            {courses.map((course) => (
+              <SwiperSlide
+                key={course.id}
+                className="aspect-video w-full overflow-hidden border-4 border-primary dark:border-primaryDark sm:w-[min(578px,100%)] md:rounded"
+              >
+                <CourseFigure course={course} />
+              </SwiperSlide>
+            ))}
+          </ComingSoon>
+        </GenericSwiper>
+      </motion.div>
+      <motion.div className="relative z-10" variants={itemVariants}>
+        <GenericSwiper moreHref={localArticlesRoute("tips")} title="General">
+          <ComingSoon active={!tips.length}>
+            {tips.map((article) => (
+              <SwiperSlide
+                key={article.id}
+                className="aspect-video w-full overflow-hidden border-4 border-primary dark:border-primaryDark sm:w-[min(578px,100%)] md:rounded"
+              >
+                <ArticleFigure article={article} />
+              </SwiperSlide>
+            ))}
+          </ComingSoon>
+        </GenericSwiper>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default Slides;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import type {ArticleModel, CourseModel} from '@/types/ModelTypes';
 
 import Slides from '@/app/(components)/Slides';
-import {ApiRouteArticles, ApiRouteCourses} from '@/routes/api-routes';
 
 import type {SuccessResponse} from '@/functions/shared/api';
 import api from '@/functions/shared/api';
 
 const getArticles = async () => {
-    const { data: { articles: general } } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>(ApiRouteArticles('general'));
-    const { data: { articles: tips } } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>(ApiRouteArticles('tips'));
-    const { data: { courses } } = await api.simpleGet<null, SuccessResponse<{ courses: CourseModel[] }>>(ApiRouteCourses());
+    const { data: { articles: general } } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>('/articles/general');
+    const { data: { articles: tips } } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>('/articles/tips');
+    const { data: { courses } } = await api.simpleGet<null, SuccessResponse<{ courses: CourseModel[] }>>('/courses');
+
     return { general, courses, tips };
 
 };
