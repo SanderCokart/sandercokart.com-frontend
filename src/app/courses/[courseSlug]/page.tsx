aa7f9ac04b13c5ccade3a693afdc19e285de15ff
Sander Cokart <cokart32@gmail.com>
2024-01-12 22:39:59 +0100

    Update course page component layout and data fetching
    The course page component has been updated to include more detailed information about the course, such as article count, banner, creation and update dates, slug, and title. This update also includes changes to the data fetching functionality, from fetching articles to fetching specific course details. A new layout has been implemented to display this additional information.

--- src/app/courses/[courseSlug]/page.tsx
+++ src/app/courses/[courseSlug]/page.tsx
@@ -1,30 +1,67 @@
-import type {CourseModel} from '@/types/ModelTypes';
-import type {ArticleModelsResponse} from '@/types/ResponseTypes';
+import type { CourseModel } from "@/types/ModelTypes";
+import type { CourseModelResponse } from "@/types/ResponseTypes";
 
-import {ApiRouteCourse} from '@/routes/api-routes';
+import { ArticleFigure } from "@/app/[articleType]/ArticleFigure";
+import { ApiRouteCourse } from "@/routes/api-routes";
+import { twJoin } from "tailwind-merge";
 
-import type {SuccessResponse} from '@/functions/shared/api';
-import api from '@/functions/shared/api';
+import type { SuccessResponse } from "@/functions/shared/api";
+import api from "@/functions/shared/api";
 
 interface PageProps {
-    params: {
-        courseSlug: string;
-    };
+  params: {
+    courseSlug: string;
+  };
 }
 
-const getCourse = async (slug: CourseModel['slug']) => {
-    const { data: { articles } } = await api.simpleGet<null, SuccessResponse<ArticleModelsResponse>>(ApiRouteCourse(slug));
-    return articles;
+const getCourse = async (slug: CourseModel["slug"]) => {
+  const {
+    data: { course },
+  } = await api.simpleGet<null, SuccessResponse<CourseModelResponse>>(
+    ApiRouteCourse(slug),
+  );
+
+  return course;
 };
 
-const CoursePage = ({ params: { courseSlug } }: PageProps) => {
-    const articles = getCourse(courseSlug);
+const CoursePage = async ({ params: { courseSlug } }: PageProps) => {
+  const {
+    articles,
+    articles_count,
+    banner,
+    created_at,
+    description,
+    id,
+    published_at,
+    slug,
+    title,
+    updated_at,
+  } = await getCourse(courseSlug);
 
-    return (
-        <main className="min-h-main">
-
-        </main>
-    );
+  return (
+    <div className="flex">
+      <main>
+        <div className="grid grid-cols-3 place-items-center bg-secondary dark:bg-secondaryDark">
+          <h1 className="col-start-2 text-center font-digital text-4xl font-bold ">
+            {title}
+          </h1>
+          <div className="font-digital">
+            {articles_count} article{articles_count > 1 ? "s" : ""}
+          </div>
+        </div>
+        <div
+          className={twJoin(
+            "pointer-events-none grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5",
+            "[&:hover_>*:hover]:opacity-100 [&:hover_>*]:opacity-75 [&_>*]:transition-opacity",
+          )}
+        >
+          {articles.map((article) => (
+            <ArticleFigure key={article.id} article={article} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default CoursePage;
