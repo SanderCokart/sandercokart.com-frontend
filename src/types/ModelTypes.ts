40769cc7a16c3631996464cff6f2986f80ea58fa
Sander Cokart <cokart32@gmail.com>
2024-01-12 22:39:41 +0100

    Refactor code formatting in ModelTypes
    The changes focused on enhancing code readability by restructuring the format in the 'ModelTypes.ts' file. The adjustments made include tidying up spaces in 'import' statements and improving the layout of interfaces for better clarity.

--- src/types/ModelTypes.ts
+++ src/types/ModelTypes.ts
@@ -1,80 +1,80 @@
-import type {ArticleType} from '@/types/CommonTypes';
+import type { ArticleType } from "@/types/CommonTypes";
 
 export interface ArticleTypeModel {
-    id: number;
-    name: ArticleType;
+  id: number;
+  name: ArticleType;
 }
 
 interface MediaModel {
-    //required
-    id: number;
-    model_type: string;
-    model_id: number;
-    disk: string;
-    file_name: string;
-    original_url: string;
-    preview_url: string;
-    //optional
-    uuid?: string;
-    collection_name?: string;
-    name?: string;
-    mime_type?: string;
-    conversions_disk?: string;
-    size?: number;
-    manipulations?: any[];
-    custom_properties?: any[];
-    generated_conversions?: any[];
-    responsive_images?: any[];
-    order_column?: number;
-    created_at?: Date;
-    updated_at?: Date;
+  //required
+  id: number;
+  model_type: string;
+  model_id: number;
+  disk: string;
+  file_name: string;
+  original_url: string;
+  preview_url: string;
+  //optional
+  uuid?: string;
+  collection_name?: string;
+  name?: string;
+  mime_type?: string;
+  conversions_disk?: string;
+  size?: number;
+  manipulations?: any[];
+  custom_properties?: any[];
+  generated_conversions?: any[];
+  responsive_images?: any[];
+  order_column?: number;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
 export interface BaseArticleModel {
-    id: number;
-    title: string;
-    slug: string;
-    created_at?: string;
-    updated_at?: string;
-    published_at: string;
-    banner: MediaModel;
-
+  id: number;
+  title: string;
+  slug: string;
+  created_at?: string;
+  updated_at?: string;
+  published_at: string;
+  banner: MediaModel;
 }
 
 export interface ArticleModel extends BaseArticleModel {
-    excerpt: string;
-    body: string;
-    article_type_id: number;
-    type: ArticleTypeModel;
+  excerpt: string;
+  body: string;
+  article_type_id: number;
+  type: ArticleTypeModel;
 }
 
 export interface CourseModel {
-    id: number;
-    title: string;
-    description: string;
-    published_at: string;
-    created_at: string;
-    updated_at: string;
-    articles_count: number;
-    slug: string;
-    banner: MediaModel;
+  id: number;
+  title: string;
+  description: string;
+  published_at: string;
+  created_at: string;
+  updated_at: string;
+  articles_count: number;
+  slug: string;
+  banner: MediaModel;
+  articles: ArticleModel[];
 }
 
 export interface Models {
-    articles: {
-        model: ArticleModel;
-        routeParams: {
-            type: ArticleType;
-            slug: string;
-        }
+  articles: {
+    model: ArticleModel;
+    routeParams: {
+      type: ArticleType;
+      slug: string;
     };
-    courses: {
-        model: CourseModel;
-        routeParams: {
-            slug: string;
-        }
+  };
+  courses: {
+    model: CourseModel;
+    routeParams: {
+      slug: string;
     };
-    media: {
-        model: MediaModel;
-    };
+  };
+  media: {
+    model: MediaModel;
+  };
 }
