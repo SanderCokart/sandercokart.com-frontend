8ca5ef0a33c0624d0da94289ae4a769f3251b321
Sander Cokart <cokart32@gmail.com>
2024-01-12 22:39:31 +0100

    Refactor Header component for better readability
    The header component's syntax has been cleaned up by adding proper spacing and indentation for better readability. Minor changes in the class applications also have been done to enhcance the visual experience. This refactoring does not affect the component's functionality but makes the code easier to read and maintain.

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,20 +1,26 @@
-import DesktopNavigation from '@/app/(components)/(navigation)/DesktopNavigation';
-import {localHomeRoute} from '@/routes/local-routes';
-import Link from 'next/link';
+import DesktopNavigation from "@/app/(components)/(navigation)/DesktopNavigation";
+import { localHomeRoute } from "@/routes/local-routes";
+import Link from "next/link";
 
-import {ThemeToggle} from '@/components/ThemeToggle';
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const Header = () => (
-    <section
-        className="mx-auto grid max-w-screen-xl grid-cols-[auto,40px] place-items-center gap-4 overflow-hidden px-4 py-2 font-digital font-bold text-white sm:min-h-[68px] md:grid-cols-[auto,1fr,40px]">
-        <h1 className="justify-self-start whitespace-nowrap">
-            <Link className="group flex flex-col text-base drop-shadow-lg" href={localHomeRoute('hero')}>
-                <span className="block text-base !leading-none transition-colors  group-hover:text-secondary  sm:text-3xl">sandercokart.com</span>
-                <span className="block self-end text-xs !leading-none  transition-colors sm:text-xl">Let's Learn...</span>
-            </Link>
-        </h1>
-        <DesktopNavigation className="hidden flex-wrap justify-center gap-x-8 gap-y-1 justify-self-center text-2xl md:flex"/>
-        <ThemeToggle className="mx-2 justify-self-end text-2xl"/>
-    </section>
+  <section className="mx-auto grid max-w-screen-xl grid-cols-[auto,40px] place-items-center gap-4 overflow-hidden px-4 py-2 font-digital font-bold text-white sm:min-h-[68px] md:grid-cols-[auto,1fr,40px]">
+    <h1 className="justify-self-start whitespace-nowrap">
+      <Link
+        className="group flex flex-col text-base drop-shadow-lg"
+        href={localHomeRoute("hero")}
+      >
+        <span className="block text-base !leading-none transition-colors  group-hover:text-secondary dark:group-hover:text-secondaryDark  sm:text-3xl">
+          sandercokart.com
+        </span>
+        <span className="block self-end text-xs !leading-none  transition-colors sm:text-xl">
+          Let's Learn...
+        </span>
+      </Link>
+    </h1>
+    <DesktopNavigation className="hidden flex-wrap justify-center gap-x-8 gap-y-1 justify-self-center text-2xl md:flex" />
+    <ThemeToggle className="mx-2 justify-self-end text-2xl" />
+  </section>
 );
 export default Header;
