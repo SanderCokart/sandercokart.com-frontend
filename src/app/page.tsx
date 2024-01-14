41f3fbee70eea6cd0595a326cfdfa25255d1e4e4
Sander Cokart <cokart32@gmail.com>
2024-01-14 23:30:56 +0100

    Refactor code formatting and remove unused component
    The code has been refactored to enhance readability and improved spacing for more clarity. The Slides component, which was previously imported but not otherwise used, has been removed from the HomePage function.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
-import type {ArticleModel, CourseModel} from '@/types/ModelTypes';
+import type { ArticleModel, CourseModel } from "@/types/ModelTypes";
 
-import Slides from '@/app/(components)/Slides';
-
-import type {SuccessResponse} from '@/functions/shared/api';
-import api from '@/functions/shared/api';
+import type { SuccessResponse } from "@/functions/shared/api";
+import api from "@/functions/shared/api";
 
 const getArticles = async () => {
-    const { data: { articles: general } } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>('/articles/general');
-    const { data: { articles: tips } } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>('/articles/tips');
-    const { data: { courses } } = await api.simpleGet<null, SuccessResponse<{ courses: CourseModel[] }>>('/courses');
+  const {
+    data: { articles: general },
+  } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>(
+    "/articles/general",
+  );
+  const {
+    data: { articles: tips },
+  } = await api.simpleGet<null, SuccessResponse<{ articles: ArticleModel[] }>>(
+    "/articles/tips",
+  );
+  const {
+    data: { courses },
+  } = await api.simpleGet<null, SuccessResponse<{ courses: CourseModel[] }>>(
+    "/courses",
+  );
 
-    return { general, courses, tips };
-
+  return { general, courses, tips };
 };
 
 const HomePage = async () => {
-    const articles = await getArticles();
+  const articles = await getArticles();
 
-    return (
-        <main className="min-h-main flex flex-col">
-            <Slides articles={articles}/>
-        </main>
-    );
+  return <main className=""></main>;
 };
 
 export default HomePage;
