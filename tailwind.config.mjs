d0feb4bd04d4399e5be5043465a04f09dd64d9f6
Sander Cokart <cokart32@gmail.com>
2024-01-14 15:58:04 +0100

    Refactor tailwind configuration file
    The existing tailwind configuration file has been deleted, and replaced with a new, streamlined, modular configuration file. This new setup simplifies our config and makes it a bit more understandable. It also switches to using the '.mjs' extension for improved ES modules support.

---
+++ tailwind.config.mjs
@@ -0,0 +1,63 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
+import forms from "@tailwindcss/forms";
+
+const tailwindPlugins = [typography, forms];
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-roboto)", ...defaultTheme.fontFamily.sans],
+        code: "var(--font-cascadia-mono)",
+        digital: "var(--font-digital)",
+      },
+      borderRadius: {
+        DEFAULT: "var(--radius)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        "background-foreground": "hsl(var(--background-foreground))",
+
+        muted: "hsl(var(--muted))",
+        "muted-foreground": "hsl(var(--muted-foreground))",
+
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+
+        primary: "hsl(var(--primary))",
+        "primary-foreground": "hsl(var(--primary-foreground))",
+
+        secondary: "hsl(var(--secondary))",
+        "secondary-foreground": "hsl(var(--secondary-foreground))",
+
+        accent: "hsl(var(--accent))",
+        "accent-foreground": "hsl(var(--accent-foreground))",
+
+        error: "hsl(var(--error))",
+        "error-foreground": "hsl(var(--error-foreground))",
+
+        warning: "hsl(var(--warning))",
+        "warning-foreground": "hsl(var(--warning-foreground))",
+
+        success: "hsl(var(--success))",
+        "success-foreground": "hsl(var(--success-foreground))",
+
+        info: "hsl(var(--info))",
+        "info-foreground": "hsl(var(--info-foreground))",
+
+        react: "#61dafb",
+        laravel: "#ff2d20",
+        vue: "#42b883",
+      },
+    },
+  },
+  plugins: tailwindPlugins,
+};
