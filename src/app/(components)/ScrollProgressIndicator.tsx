f520d718e702d2b3e7a7729d2a4f937fd815d1f2
Sander Cokart <cokart32@gmail.com>
2024-01-14 23:30:39 +0100

    Refactor ScrollProgressIndicator component
    The ScrollProgressIndicator component has been refactored for improved readability and consistency. Changes include syntax and formatting adjustments, such as enhancing string declarations and reformating the return statement for better clarity.

--- src/app/(components)/ScrollProgressIndicator.tsx
+++ src/app/(components)/ScrollProgressIndicator.tsx
@@ -1,11 +1,13 @@
-'use client';
-import {useScroll, motion} from 'framer-motion';
+"use client";
+import { useScroll, motion } from "framer-motion";
 
 const ScrollProgressIndicator = () => {
-    const { scrollYProgress } = useScroll();
-    return (
-        <motion.div className="absolute -bottom-0 h-1 w-full bg-secondary dark:bg-secondaryDark"
-                    style={{ scaleX: scrollYProgress }}/>
-    );
+  const { scrollYProgress } = useScroll();
+  return (
+    <motion.div
+      className="dark:bg-secondaryDark absolute -bottom-0 h-1 w-full bg-secondary"
+      style={{ scaleX: scrollYProgress }}
+    />
+  );
 };
-export default ScrollProgressIndicator
+export default ScrollProgressIndicator;
