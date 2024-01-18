422211d23e8f35c0fc7d007459df1899590783b9
Sander Cokart <cokart32@gmail.com>
2024-01-18 22:00:51 +0100

    Refactor local-routes.ts for readability
    The local-routes.ts file has been updated to enhance code readability. The changes include reformatting of the code structures to make them easier to follow, as well as the addition of editor-fold tags to better organize the section for Articles and Courses.

--- src/routes/local-routes.ts
+++ src/routes/local-routes.ts
@@ -1,12 +1,23 @@
-import type {ArticleType} from '@/types/CommonTypes';
-import type {CourseModel, ArticleModel} from '@/types/ModelTypes';
+import type { ArticleType } from "@/types/CommonTypes";
+import type { CourseModel, ArticleModel } from "@/types/ModelTypes";
 
-export const localHomeRoute = (hash: string) => hash ? `/#${hash}` : '/';
+export const localHomeRoute = () => "/";
 
-//articles
-export const localArticleRoute = (typeName: ArticleType, slug: ArticleModel['slug']) => `/${typeName}/${slug}`;
+//<editor-fold desc="Articles">
+export const localArticleRoute = (
+  typeName: ArticleType,
+  slug: ArticleModel["slug"],
+) => `/${typeName}/${slug}`;
+
 export const localArticlesRoute = (typeName: ArticleType) => `/${typeName}`;
-//courses
-export const localCourseRoute = (courseSlug: CourseModel['slug']) => `/courses/${courseSlug}`;
-export const localCourseArticleRoute = (courseSlug: CourseModel['slug'], articleSlug: ArticleModel['slug']) => `/courses/${courseSlug}/${articleSlug}`;
-export const localCoursesRoute = () => '/courses';
+//</editor-fold>
+
+//<editor-fold desc="Courses">
+export const localCourseRoute = (courseSlug: CourseModel["slug"]) =>
+  `/courses/${courseSlug}`;
+export const localCourseArticleRoute = (
+  courseSlug: CourseModel["slug"],
+  articleSlug: ArticleModel["slug"],
+) => `/courses/${courseSlug}/${articleSlug}`;
+export const localCoursesRoute = () => "/courses";
+//</editor-fold>
