fc6670e55d5bc7d7efc65308a1df59fa9b5ceeb5
Sander Cokart <cokart32@gmail.com>
2024-01-21 15:57:23 +0100

    Swap ascend and descend animations
    The "ascendVariant" and "descendVariant" animations were incorrectly placed. These were swapped to match the relevant animation with the correct variable name. Also added an 'easeIn' transition effect.

--- src/constants/animations/ascendAndDecend.ts
+++ src/constants/animations/ascendAndDecend.ts
@@ -1,11 +1,11 @@
-export const ascendVariant = {
-    hidden: { opacity: 0, y: 100, transition: { duration: .25, y: { duration: .5 } } },
-    visible: { opacity: 1, y: 0, transition: { duration: .25, y: { duration: .5 } } },
-    exit: { opacity: 0, y: 100, transition: { duration: .25, y: { duration: .5 } } }
+export const descendVariant = {
+  hidden: { opacity: 0, y: 100, transition: { duration: 0.25, y: { duration: 0.5, ease: 'easeIn' } } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.25, y: { duration: 0.5, ease: 'easeIn' } } },
+  exit: { opacity: 0, y: 100, transition: { duration: 0.25, y: { duration: 0.5, ease: 'easeIn' } } },
 };
 
-export const descendVariant = {
-    hidden: { opacity: 0, y: -100, transition: { duration: .25, y: { duration: .5 } } },
-    visible: { opacity: 1, y: 0, transition: { duration: .25, y: { duration: .5 } } },
-    exit: { opacity: 0, y: -100, transition: { duration: .25, y: { duration: .5 } } }
+export const ascendVariant = {
+  hidden: { opacity: 0, y: -100, transition: { duration: 0.25, y: { duration: 0.5, ease: 'easeIn' } } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.25, y: { duration: 0.5, ease: 'easeIn' } } },
+  exit: { opacity: 0, y: -100, transition: { duration: 0.25, y: { duration: 0.5, ease: 'easeIn' } } },
 };
