706018de3c66e9492d32a0c41631a952f56e9768
Sander Cokart <cokart32@gmail.com>
2024-01-21 15:58:26 +0100

    Refactor ArticleFigure component for clarity
    The code has been refactored to improve readability and eliminate unnecessary complexity in the ArticleFigure.tsx file. The changes include rearrangement of imports and reformatting of element structures. This simplification enhances maintainability.

--- src/app/[articleType]/ArticleFigure.tsx
+++ src/app/[articleType]/ArticleFigure.tsx
@@ -1,11 +1,12 @@
-import type { ArticleModel } from "@/types/ModelTypes";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { localArticleRoute } from "@/routes/local-routes";
-import Image from "next/image";
-import Link from "next/link";
+import type { ArticleModel } from '@/types/ModelTypes';
 
-import calculatePublishedTimestamp from "@/functions/shared/calculatePublishedTimestamp";
-import { cn } from "@/functions/shared/utils";
+import calculatePublishedTimestamp from '@/functions/shared/calculatePublishedTimestamp';
+import { cn } from '@/functions/shared/utils';
+
+import { localArticleRoute } from '@/routes/local-routes';
 
 interface ArticleFigureProps {
   article: ArticleModel;
@@ -15,51 +16,37 @@ export const ArticleFigure = ({ article }: ArticleFigureProps) => (
   <Link
     key={article.id}
     className="group pointer-events-auto"
-    href={localArticleRoute(article.type.name, article.slug)}
-  >
-    <figure className="relative flex flex-col border-2 border-secondary transition-transform hover:scale-95 dark:border-secondaryDark">
+    href={localArticleRoute(article.type.name, article.slug)}>
+    <figure className="dark:border-secondaryDark relative flex flex-col border-2 border-secondary transition-transform hover:scale-95">
       <div className="relative aspect-video h-full w-full overflow-hidden">
         <Image
           fill
           alt={article.title}
           className="transition-transform group-hover:scale-110"
           src={article.banner.original_url}
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: 'cover' }}
         />
       </div>
 
-      <figcaption
-        className={cn(
-          "flex h-full flex-col justify-between",
-          "bg-black/25 text-white",
-        )}
-      >
+      <figcaption className={cn('flex h-full flex-col justify-between bg-black/25 text-white')}>
         <h1
           className={cn(
-            "label bg-secondary transition-opacity dark:bg-secondaryDark",
-            "line-clamp-2 font-code text-sm font-black capitalize text-black dark:text-white md:text-xl",
-          )}
-        >
+            'bg-secondary text-secondary-foreground',
+            'font-code text-sm font-black capitalize  md:text-xl',
+            'line-clamp-2',
+          )}>
           {article.title}
         </h1>
 
         <div
           className={cn(
-            "flex flex-col p-4",
-            "bg-bodyLightSecondary text-black dark:bg-bodyDarkSecondary dark:text-white",
-          )}
-        >
-          <p className="line-clamp-5 font-code text-xs font-normal md:text-base">
-            {article.excerpt}
-          </p>
+            'flex flex-col p-4',
+            'bg-bodyLightSecondary dark:bg-bodyDarkSecondary text-black dark:text-white',
+          )}>
+          <p className="line-clamp-5 font-code text-xs font-normal md:text-base">{article.excerpt}</p>
         </div>
 
-        <span
-          className={cn(
-            "label shadow-none",
-            "text-center font-code text-xs text-black",
-          )}
-        >
+        <span className={cn('shadow-none', 'text-center font-code text-xs text-black')}>
           Published: {calculatePublishedTimestamp(article.published_at, true)}
         </span>
       </figcaption>
