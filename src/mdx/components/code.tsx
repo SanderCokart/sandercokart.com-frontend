7861100109e6930c65805dd8eb1635e82246f8da
Sander Cokart <cokart32@gmail.com>
2024-03-17 02:43:38 +0100

    Refactor CodeTabs component to be more versatile
    This commit refactors the CodeTabs component to accept children props, thereby making it more reusable. It removes hardcoded values and replaces them with dynamic ones based on the props it receives. The changes also include type imports for better type safety.

--- src/mdx/components/code.tsx
+++ src/mdx/components/code.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+'use client';
 
-import type { HTMLAttributes } from 'react';
+import React, { Children } from 'react';
+
+import type { HTMLAttributes, ReactNode } from 'react';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
@@ -15,31 +17,27 @@ const Code = ({ children, title, className, ...restOfProps }: HTMLAttributes<HTM
   );
 };
 
-const CodeTabs = ({}: {}) => (
-  <Tabs className="bg-muted" defaultValue="index.html">
+const CodeTabs = ({ children }: { children: ReactNode }) => (
+  <Tabs defaultValue="index.html">
     <TabsList className="m-0 flex h-auto w-min divide-x-2 divide-muted-foreground p-0">
-      <TabsTrigger
-        className="grow rounded-none bg-muted text-muted-foreground data-[state=active]:bg-secondary data-[state=active]:text-secondary-foreground"
-        value="index.html">
-        index.html
-      </TabsTrigger>
-      <TabsTrigger
-        className="grow rounded-none bg-muted text-muted-foreground data-[state=active]:bg-secondary data-[state=active]:text-secondary-foreground"
-        value="app.js">
-        app.js
-      </TabsTrigger>
-      <TabsTrigger
-        className="grow rounded-none bg-muted text-muted-foreground data-[state=active]:bg-secondary data-[state=active]:text-secondary-foreground"
-        value="globals.css">
-        globals.css
-      </TabsTrigger>
+      {Children.map(children, child => (
+        <TabsTrigger
+          className="grow rounded-none bg-muted text-muted-foreground data-[state=active]:bg-secondary data-[state=active]:text-secondary-foreground"
+          value={(child as any).props.title}>
+          {(child as any).props.title}
+        </TabsTrigger>
+      ))}
     </TabsList>
-    <TabsContent value="index.html">index.html</TabsContent>
-    <TabsContent value="app.js">app.js</TabsContent>
-    <TabsContent value="globals.css">globals.css</TabsContent>
+    {children}
   </Tabs>
 );
 
-const CodeTab = () => null;
+const CodeTab = ({ children, title }: { children: ReactNode; title: string }) => {
+  return (
+    <TabsContent className="mt-0" value={title}>
+      {children}
+    </TabsContent>
+  );
+};
 
 export { Code, CodeTabs, CodeTab };
