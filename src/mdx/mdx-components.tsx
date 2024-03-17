f07d73e37d31c003ff929fe305183d184be526b5
Sander Cokart <cokart32@gmail.com>
2024-03-17 03:44:04 +0100

    Add and refine UI table components
    This update includes the creation of new UI components for table presentation. Several elements such as Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, and TableCaption have been added. Also, packages have been updated in the lock file to ensure compatibility and functionality of the new components.

--- src/mdx/mdx-components.tsx
+++ src/mdx/mdx-components.tsx
@@ -1,10 +1,6 @@
-import 'highlight.js/styles/tokyo-night-dark.css';
-
-import hljs from 'highlight.js';
-
 import React from 'react';
 
-import type { ComponentProps, HTMLAttributes } from 'react';
+import type { ComponentProps, HTMLAttributes, TableHTMLAttributes, TdHTMLAttributes, ThHTMLAttributes } from 'react';
 /*@ts-expect-error - MDXProvider is not typed*/
 import type { MDXProvider } from '@mdx-js/react';
 
@@ -12,21 +8,35 @@ import { cn } from '@/functions/shared/utils';
 
 import { Code, CodeTab, CodeTabs } from './components/code';
 
+import './code.css';
+
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+
 const mdxComponents: ComponentProps<typeof MDXProvider>['components'] = {
+  table: (props: TableHTMLAttributes<HTMLTableElement>) => <Table {...props} />,
+  thead: (props: HTMLAttributes<HTMLTableSectionElement>) => <TableHeader className="" {...props} />,
+  tbody: (props: HTMLAttributes<HTMLTableSectionElement>) => <TableBody {...props} />,
+  tfoot: (props: HTMLAttributes<HTMLTableSectionElement>) => <TableFooter {...props} />,
+  tr: (props: HTMLAttributes<HTMLTableRowElement>) => (
+    <TableRow {...props} className="bg-primary text-primary-foreground hover:bg-accent hover:text-accent-foreground" />
+  ),
+  th: (props: ThHTMLAttributes<HTMLTableCellElement>) => (
+    <TableHead className="bg-secondary text-secondary-foreground" {...props} />
+  ),
+  td: (props: TdHTMLAttributes<HTMLTableCellElement>) => <TableCell {...props} />,
+
   Code,
   CodeTab,
   CodeTabs,
   pre: (props: any) => <pre className="hljs overflow-auto border border-secondary p-4" {...props} />,
   code: ({ children, className, ...props }: HTMLAttributes<HTMLElement>) => {
-    const language = className?.replace('language-', '') as string;
-    const highlightedCode = hljs.highlight(children as string, { language });
+    // const language = className?.replace('language-', '') as string;
+    // const highlightedCode = hljs.highlight(children as string, { language });
 
     return (
-      <code
-        {...props}
-        className={cn(className, 'font-code')}
-        dangerouslySetInnerHTML={{ __html: highlightedCode.value }}
-      />
+      <code {...props} className={cn(className, 'font-code')}>
+        {children}
+      </code>
     );
   },
 };
--- src/mdx/mdx-options.ts
+++ src/mdx/mdx-options.ts
@@ -1,9 +1,18 @@
+import rehypePrism from 'rehype-prism-plus';
 import remarkGfm from 'remark-gfm';
 
 import type { SerializeOptions } from 'next-mdx-remote/dist/types';
 
 const mdxOptions: SerializeOptions['mdxOptions'] = {
   remarkPlugins: [remarkGfm],
+  rehypePlugins: [
+    [
+      rehypePrism as any,
+      {
+        showLineNumbers: true,
+      },
+    ],
+  ],
 };
 
 export default mdxOptions;
